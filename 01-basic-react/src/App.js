e0ee8f84b1b17e96015a5804397daab73e7f7c8a
saketkumar123 <saket.ulsi@gmail.com>
2025-12-21 23:02:43 +0530

    React file cleanup

---
+++ 01-basic-react/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Hello React Js by Chai Aur Code</h1>
+  );
+}
+
+export default App;
---
+++ 01-vite-react/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+
+  return (
+    <h1>Hello React using vite library</h1>
+  )
+}
+
+export default App
