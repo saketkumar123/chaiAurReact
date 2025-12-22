6ebd09791d395b86a983922f49cd20a151f5b206
saketkumar123 <saket.ulsi@gmail.com>
2025-12-22 13:13:26 +0530

    Custom React Added

--- 01-vite-react/src/main.jsx
+++ 01-vite-react/src/main.jsx
@@ -1,9 +1,44 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react'
+
+function MyApp() {
+  return (
+    <div>
+      <h1>My Custom App!</h1>
+    </div>
+  )
+}
+
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//     href: 'https://google.com',
+//     target: '_blank'
+//   },
+//   children: 'click me to visit google'
+// }
+
+const anotherElement = (
+  <a href='https://google.com' target='_blank'>Visit Google</a>
+)
+
+const anotherUser = 'Chai aur my react'
+
+const reactElement = React.createElement(
+  'a',
+  { href: 'https://google.com', target: '_blank' },
+  'click me to visit google',
+
+  anotherUser
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* MyApp - Not Recommend*/}
+    {/* <anotherElement /> */}
+    reactElement
   </StrictMode>,
 )
