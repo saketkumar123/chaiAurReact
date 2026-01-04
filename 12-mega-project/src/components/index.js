980440c42c20aa717950ac4e854550d6c3974499
saketkumar123 <saket.ulsi@gmail.com>
2026-01-05 01:11:21 +0530

    Added RTE and PostForm Component

--- 12-mega-project/src/components/index.js
+++ 12-mega-project/src/components/index.js
@@ -3,10 +3,27 @@ import Footer from './Footer/Footer'
 import Container from './container/Container'
 import Logo from './Logo'
 import LogoutBtn from './Header/LogoutBtn'
+import RTE from './RTE'
+import Signup from './Signup'
+import Login from './Login'
+import Button from './Button'
+import PostCard from './PostCard'
+import AuthLayout from './AuthLayout'
+import Input from './Input'
+import PostForm from './post-form/PostForm'
+
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    Button,
+    PostCard,
+    AuthLayout,
+    Input,
+    PostForm
 }
